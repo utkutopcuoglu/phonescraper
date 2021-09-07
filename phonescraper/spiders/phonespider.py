170542cea8ffa925a0912948263ed820444c5e32
Utku <utkutopcuoglu@novit.ai>
2021-09-07 17:04:07 +0300

    ADD skip page functionality.

--- phonescraper/spiders/phonespider.py
+++ phonescraper/spiders/phonespider.py
@@ -8,8 +8,14 @@ class PhoneSpider(scrapy.Spider):
     #in built funcitons, response contains the response of the site.
     def parse(self, response):
         for product_descriptions in response.css("div.p-card-chldrn-cntnr"):
+            # you can do try except here if necessary.
             yield {
                 "product":product_descriptions.css("div.prdct-desc-cntnr-name.hasRatings::text").get(),
                 "price":product_descriptions.css("div.prc-box-sllng.prc-box-sllng-w-dscntd::text").get(),
                 "image":product_descriptions.css("img.p-card-img").attrib["src"]
             }
+
+        # Hyptotethical function: If the next page exists, go to it.
+        next_page = response.css("a.action.next").attrib["href"]
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse) # Calls the function back.
